f2c8caaeba94c17dd088cbe8c4323ecd5f85fdff
mrelectrq <tiora100bass@gmail.com>
2020-04-12 13:52:11 +0300

    Laborator3

---
+++ Laborator3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            RVT_ApiRequest resp = new RVT_ApiRequest();
+        Start:
+            Console.WriteLine("Choose HTTP method request : \n\n 1.GET METHOD \n\n 2. POST METHOD \n\n 3. HEAD METHOD \n\n 4. OPTIONS METHOD \n\n 5. Exit...");
+            string input = Console.ReadLine();
+            int option;
+
+           
+            if (int.TryParse(input, out option))
+               
+            switch (option)
+                {
+                    case 1:
+
+
+                        Console.WriteLine(resp.GetMeth());
+                        break;
+                    case 2:
+                        Console.WriteLine(resp.POSTMeth());
+                        break;
+                    case 3:
+                        Console.WriteLine(resp.HeadMeth());
+                        break;
+
+                    case 4:
+                        Console.WriteLine(resp.OptionMeth());
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid");
+                        break;
+                }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+
+            goto Start;
+
+        }
+    }
+}
---
+++ Laborator3/RVT_ApiRequest.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator3
+{
+    public class RVT_ApiRequest
+    {
+        private static HttpClientHandler handler = new HttpClientHandler();
+        private static HttpClient client;
+       public RVT_ApiRequest()
+        {
+            var cert = new X509Certificate2(Path.Combine
+    ("D:\\C#_projects\\Remote_Vote_System\\Certs\\WebCert", "web-certificate.pfx"), "ar4iar4i");
+            //var proxy = new WebProxy()
+            //{
+            //    Address = new Uri($"http://50.226.151.5:80"),
+            //    BypassProxyOnLocal = false,
+            //    UseDefaultCredentials = true,
+            //};   
+
+
+            handler.ClientCertificates.Add(cert);
+            handler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            handler.AllowAutoRedirect = true;
+            //handler.Proxy = proxy;
+            client = new HttpClient(handler);
+            client.BaseAddress = new Uri("https://localhost:44383/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            handler.CookieContainer.Add(new Cookie("session_id", "4eec00020f8b76651015fb7118b63b02","/"));
+            
+        }
+
+        public string OptionMeth()
+        {
+            var request = new HttpRequestMessage()
+            {
+
+                Method = HttpMethod.Options,
+
+            };
+            var response = client.SendAsync(request);
+
+            try
+            {
+                var response_message = response.Result.Headers;
+                return response_message.ToString();
+            }
+            catch (AggregateException e)
+            {
+                return "Eroare de raspuns";
+            }
+
+        }
+
+        public string GetMeth()
+        {
+            var response = client.GetAsync("api/Register");
+            try
+            {
+                var response_message = response.Result.Content.ReadAsStringAsync();
+                return response_message.Result;
+            }
+            catch (AggregateException e) 
+            {
+                return "Eroare de raspuns";
+            }
+        }
+        public string POSTMeth()
+        {
+            var request_message = new StringContent("data", Encoding.UTF8, "application/json");
+            var response = client.PostAsync("api/Register", request_message);
+            
+            try
+            {
+               var response_message = response.Result.Content.ReadAsStringAsync();
+                return response_message.Result;  
+            }
+            catch(AggregateException e)
+            {
+                return "Eroare de raspuns";
+            }
+
+        }
+        public string HeadMeth()
+        {
+            var request = new HttpRequestMessage()
+            {
+
+                Method = HttpMethod.Head,
+
+            };
+            var response = client.SendAsync(request);
+
+            try
+            {
+                var response_message = response.Result.Headers;
+                return response_message.ToString();
+            }
+            catch (AggregateException e)
+            {
+                return "Eroare de raspuns";
+            }
+        }
+
+    }
+}
